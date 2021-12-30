8dfe6b18d83588a2b43c47a41d3b09a9c1240cb3
Angus Johnson <AngusJohnson@users.noreply.github.com>
2021-12-30 12:41:57 +1000

    Bugfixes

--- Vcl.Imaging.QOI.pas
+++ Vcl.Imaging.QOI.pas
@@ -4,8 +4,8 @@ interface
 
 (*******************************************************************************
 * Author    :  Angus Johnson                                                   *
-* Version   :  0.99.3                                                          *
-* Date      :  28 December 2021                                                *
+* Version   :  1.0                                                          *
+* Date      :  29 December 2021                                                *
 * Website   :  http://www.angusj.com                                           *
 * Copyright :  Angus Johnson 2021                                              *
 *                                                                              *
@@ -39,13 +39,18 @@ uses
   Winapi.Windows,
   Vcl.Graphics,
   Vcl.ExtCtrls,
+  System.Math,
   System.Classes;
 
 type
+  TTriState = (tsUnknown, tsFalse, tsTrue);
   TQoiImage = class(TGraphic)
   private
-    FBitmap: TBitmap;
-    FSaveAsBmp: Boolean;
+    FBitmap     : TBitmap;
+    FSaveAsBmp  : Boolean;
+    FHasTransPx : TTriState;
+    FPremult    : Boolean;
+    function  GetHasTrans: Boolean;
   protected
     procedure Draw(ACanvas: TCanvas; const Rect: TRect); override;
     function GetEmpty: Boolean; override;
@@ -54,9 +59,11 @@ type
     function GetWidth: Integer; override;
     procedure SetHeight(Value: Integer); override;
     procedure SetWidth(Value: Integer); override;
+    property Image: TBitmap read FBitmap;
   public
     constructor Create; override;
     destructor Destroy; override;
+    procedure Assign(Source: TPersistent); override;
     procedure AssignTo(Dest: TPersistent); override;
     class function CanLoadFromStream(Stream: TStream): Boolean; override;
     procedure LoadFromStream(Stream: TStream); override;
@@ -67,10 +74,12 @@ type
     procedure SaveToClipboardFormat(var AFormat: Word; var AData: THandle;
       var APalette: HPALETTE); override;
     procedure SetSize(AWidth, AHeight: Integer); override;
-    property Image: TBitmap read FBitmap;
+    property HasTransparency: Boolean read GetHasTrans;
   end;
 
 implementation
+
+uses Vcl.Imaging.pngimage;
 
 ResourceString
   sQoiImageFile = 'QOI image file';
@@ -115,7 +124,7 @@ const
 
 function QOI_COLOR_HASH(c: TARGB): Byte; inline;
 begin
-  Result := (c.R * 3 + c.G * 5 + c.B * 7 + c.A * 9 + c.A * 2) mod 64;
+  Result := (c.R * 3 + c.G * 5 + c.B * 7 + c.A * 11) mod 64;
 end;
 
 function SwapBytes(Value: Cardinal): Cardinal;
@@ -147,6 +156,52 @@ begin
   inc(p);
 end;
 
+function Bmp32HasTransparency(bmp: TBitmap): Boolean;
+var
+  i : Integer;
+  c : PARGB;
+begin
+  Result := bmp.PixelFormat = pf32bit;
+  if not Result then Exit;
+  c := PARGB(bmp.ScanLine[bmp.Height -1]);
+  for i := 0 to bmp.Width * bmp.Height -1 do
+  begin
+    if c.A < 255 then Exit;
+    inc(c);
+  end;
+  Result := false;
+end;
+
+function CreateTransparentPNG(qoi: TQoiImage): TPNGImage;
+var
+  X, Y: integer;
+  src: PARGB;
+  dstAlpha: PByte;
+begin
+  Result := TPNGImage.CreateBlank(COLOR_RGBALPHA,
+    8, qoi.Width , qoi.Height);
+
+  Result.CreateAlpha;
+  Result.Canvas.CopyMode:= cmSrcCopy;
+  Result.Canvas.Draw(0, 0, qoi);
+
+  for Y := 0 to Pred(qoi.Height) do
+  begin
+    src := qoi.Image.ScanLine[Y];
+    dstAlpha := PByte(Result.AlphaScanline[Y]);
+    for X := 0 to Pred(qoi.width) do
+    begin
+      dstAlpha^ :=  src.A;
+      Inc(dstAlpha);
+      Inc(src);
+    end;
+  end;
+end;
+
+//------------------------------------------------------------------------------
+// TQoiImage methods
+//------------------------------------------------------------------------------
+
 constructor TQoiImage.Create;
 begin
   inherited;
@@ -160,18 +215,95 @@ begin
 end;
 
 procedure TQoiImage.AssignTo(Dest: TPersistent);
+var
+  tmpPng: TPngImage;
 begin
   if Dest is TQoiImage then
-    TQoiImage(Dest).Image.Assign(Image)
-  else if Dest is TBitmap then
-    TBitmap(Dest).Assign(Image)
-  else
-    inherited;
+  begin
+    TQoiImage(Dest).Image.Assign(Image);
+    TQoiImage(Dest).FHasTransPx := FHasTransPx;
+  end else if Dest is TBitmap then
+  begin
+    TBitmap(Dest).SetSize(Width, Height);
+    TBitmap(Dest).PixelFormat := pf32bit;
+    TBitmap(Dest).Canvas.Draw(0,0, self);
+  end
+  else if Dest is TPngImage then
+  begin
+    tmpPng := CreateTransparentPNG(self);
+    try
+      TPngImage(Dest).Assign(tmpPng);
+    finally
+      tmpPng.Free;
+    end;
+  end
+  else inherited;
+end;
+
+procedure TQoiImage.Assign(Source: TPersistent);
+begin
+  if Source is TQoiImage then
+  begin
+    Image.Assign(TQoiImage(Source).Image);
+    FHasTransPx := TQoiImage(Source).FHasTransPx;
+    FPremult    := TQoiImage(Source).FPremult;
+  end
+  else if Source is TBitmap then
+  begin
+    Image.Assign(Source);
+    FHasTransPx := tsUnknown;
+    FPremult    := false;
+  end else
+  begin
+    Image.Assign(Source);
+    FHasTransPx := tsUnknown;
+    FPremult    := false;
+  end;
+end;
+
+function TQoiImage.GetHasTrans: Boolean;
+begin
+  case FHasTransPx of
+    tsUnknown:
+      begin
+        Result := Bmp32HasTransparency(Image);
+        if Result then
+          FHasTransPx := tsTrue else
+          FHasTransPx := tsFalse;
+      end;
+    tsFalse: Result := false;
+    else Result := true;
+  end;
 end;
 
 procedure TQoiImage.Draw(ACanvas: TCanvas; const Rect: TRect);
+var
+  BlendFunction: TBlendFunction;
+  w, h: integer;
+  bmp: TBitmap;
 begin
-  THackedBitmap(Image).Draw(ACanvas, Rect);
+  if HasTransparency then
+  begin
+    //create a temporary Bitmap and premultiply
+    //(nb: premultiply is a prerequisite to Windows' alpha blending
+    bmp := TBitmap.create;
+    try
+      bmp.Assign(Image);
+      bmp.AlphaFormat := afPremultiplied;
+      BlendFunction.BlendOp := AC_SRC_OVER;
+      BlendFunction.AlphaFormat := AC_SRC_ALPHA;
+      BlendFunction.SourceConstantAlpha := 255;
+      BlendFunction.BlendFlags := 0;
+      w := System.Math.Min(Width, Rect.Width);
+      h := System.Math.Min(Height, Rect.Height);
+      Winapi.Windows.AlphaBlend(
+        ACanvas.Handle, Rect.Left, Rect.Top, w, h,
+        bmp.Canvas.Handle, 0, 0, w,h, BlendFunction);
+    finally
+      bmp.Free;
+    end;
+  end else
+    THackedBitmap(Image).Draw(ACanvas, Rect);
 end;
 
 function TQoiImage.GetEmpty: Boolean;
@@ -225,13 +357,13 @@ end;
 {$R-}
 procedure TQoiImage.LoadFromStream(Stream: TStream);
 var
-  size, run, vg: Integer;
+  size, run, vg, x, y: Integer;
   desc: TQOI_DESC;
   index: array [0 .. 63] of TARGB;
   px: TARGB;
   b1, b2: Byte;
   dst: PARGB;
-  src, endSrc: PByte;
+  src: PByte;
   srcTmp: TArrayOfByte;
 begin
   if not Assigned(Stream) then
@@ -258,8 +390,6 @@ begin
     Stream.Read(srcTmp[0], size);
     src := @srcTmp[0];
   end;
-  endSrc := src;
-  inc(endSrc, size - qoi_padding_size);
 
   Move(src^, desc, SizeOf(TQOI_DESC));
   inc(src, SizeOf(TQOI_DESC));
@@ -276,56 +406,59 @@ begin
   px.Color := $FF000000;
   run := 0;
   FillChar(index, SizeOf(index), 0);
-  dst := PARGB(Image.ScanLine[desc.height - 1]);
+  FHasTransPx := tsUnknown;
+  FPremult    := False;
 
-  while (src < endSrc) or (run > 0) do
+  for y := 0 to Image.height -1 do
   begin
-    if (run > 0) then
+    dst := PARGB(Image.ScanLine[y]);
+    for x := 0 to Image.Width -1 do
     begin
-      Dec(run);
-    end
-    else
-    begin
-      b1 := ReadByte(src);
-      if (b1 = QOI_OP_RGB) then
+      if (run > 0) then
       begin
-        px.R := ReadByte(src);
-        px.G := ReadByte(src);
-        px.B := ReadByte(src);
-      end
-      else if (b1 = QOI_OP_RGBA) then
+        Dec(run);
+      end else
       begin
-        px.R := ReadByte(src);
-        px.G := ReadByte(src);
-        px.B := ReadByte(src);
-        px.A := ReadByte(src);
-      end
-      else if ((b1 and QOI_MASK_2) = QOI_OP_INDEX) then
-      begin
-        px := index[b1];
-      end
-      else if (b1 and QOI_MASK_2) = QOI_OP_DIFF then
-      begin
-        px.R := px.R + ((b1 shr 4) and 3) - 2;
-        px.G := px.G + ((b1 shr 2) and 3) - 2;
-        px.B := px.B + (b1 and 3) - 2;
-      end
-      else if (b1 and QOI_MASK_2) = QOI_OP_LUMA then
-      begin
-        b2 := ReadByte(src);
-        vg := (b1 and $3F) - 32;
-        px.R := px.R + vg - 8 + ((b2 shr 4) and $F);
-        px.G := px.G + vg;
-        px.B := px.B + vg - 8 + (b2 and $F);
-      end
-      else if (b1 and QOI_MASK_2) = QOI_OP_RUN then
-        run := (b1 and $3F);
-      index[QOI_COLOR_HASH(px)] := px;
+        b1 := ReadByte(src);
+        if (b1 = QOI_OP_RGB) then
+        begin
+          px.R := ReadByte(src);
+          px.G := ReadByte(src);
+          px.B := ReadByte(src);
+        end
+        else if (b1 = QOI_OP_RGBA) then
+        begin
+          px.R := ReadByte(src);
+          px.G := ReadByte(src);
+          px.B := ReadByte(src);
+          px.A := ReadByte(src);
+        end
+        else if ((b1 and QOI_MASK_2) = QOI_OP_INDEX) then
+        begin
+          px := index[b1];
+        end
+        else if (b1 and QOI_MASK_2) = QOI_OP_DIFF then
+        begin
+          px.R := px.R + ((b1 shr 4) and 3) - 2;
+          px.G := px.G + ((b1 shr 2) and 3) - 2;
+          px.B := px.B + (b1 and 3) - 2;
+        end
+        else if (b1 and QOI_MASK_2) = QOI_OP_LUMA then
+        begin
+          b2 := ReadByte(src);
+          vg := (b1 and $3F) - 32;
+          px.R := px.R + vg - 8 + ((b2 shr 4) and $F);
+          px.G := px.G + vg;
+          px.B := px.B + vg - 8 + (b2 and $F);
+        end
+        else if (b1 and QOI_MASK_2) = QOI_OP_RUN then
+          run := (b1 and $3F);
+        index[QOI_COLOR_HASH(px)] := px;
+      end;
+      dst.Color := px.Color;
+      inc(dst);
     end;
-    dst.Color := px.Color;
-    inc(dst);
   end;
-
   Changed(Self);
 end;
 {$R+}
@@ -339,7 +472,7 @@ end;
 
 procedure TQoiImage.SaveToStream(Stream: TStream);
 var
-  i, max_size, run: Integer;
+  x,y,k,y2, max_size, run: Integer;
   vr, vg, vb, vg_r, vg_b: Integer;
   index_pos: Integer;
   bytes: TArrayOfByte;
@@ -373,79 +506,86 @@ begin
   px_prev.Color := $FF000000;
   FillChar(index, SizeOf(index), 0);
 
-  src := PARGB(Image.ScanLine[Image.height - 1]);
-  max_size := Image.width * Image.height;
-
-  for i := 1 to max_size do
+  //Images appear to be always stored bottom-up in memory
+  //(even those saved to file as top-down) and this makes
+  //pixel-parsing slightly more difficult).
+  for y := 0 to Image.height -1 do
   begin
-    if src.Color = px_prev.Color then
+    src := PARGB(Image.ScanLine[y]);
+    for x := 1 to Width do
     begin
-      inc(run);
-      if (run = 62) or (i = max_size) then
+      if src.Color = px_prev.Color then
       begin
-        qoi_write_8(dst, QOI_OP_RUN or (run - 1));
-        run := 0;
-      end;
-    end
-    else
-    begin
-      if (run > 0) then
-      begin
-        qoi_write_8(dst, QOI_OP_RUN or (run - 1));
-        run := 0;
-      end;
-
-      index_pos := QOI_COLOR_HASH(src^);
-      if (index[index_pos].Color = src.Color) then
-      begin
-        qoi_write_8(dst, QOI_OP_INDEX or index_pos);
+        inc(run);
+        if (run = 62) then
+        begin
+          qoi_write_8(dst, QOI_OP_RUN or (run - 1));
+          run := 0;
+        end;
       end
       else
       begin
-        index[index_pos] := src^;
-        if (src.A = px_prev.A) then
+        if (run > 0) then
         begin
-          vr := src.R - px_prev.R;
-          vg := src.G - px_prev.G;
-          vb := src.B - px_prev.B;
-          vg_r := vr - vg;
-          vg_b := vb - vg;
-          if ((vr > -3) and (vr < 2) and (vg > -3) and (vg < 2) and (vb > -3)
-            and (vb < 2)) then
-          begin
-            qoi_write_8(dst, QOI_OP_DIFF or (vr + 2) shl 4 or (vg + 2) shl 2 or
-              (vb + 2));
-          end
-          else if ((vg_r > -9) and (vg_r < 8) and (vg > -33) and (vg < 32) and
-            (vg_b > -9) and (vg_b < 8)) then
-          begin
-            qoi_write_8(dst, QOI_OP_LUMA or (vg + 32));
-            qoi_write_8(dst, (vg_r + 8) shl 4 or (vg_b + 8));
-          end
-          else
-          begin
-            qoi_write_8(dst, QOI_OP_RGB);
-            qoi_write_8(dst, src.R);
-            qoi_write_8(dst, src.G);
-            qoi_write_8(dst, src.B);
-          end
+          qoi_write_8(dst, QOI_OP_RUN or (run - 1));
+          run := 0;
+        end;
+
+        index_pos := QOI_COLOR_HASH(src^);
+        if (index[index_pos].Color = src.Color) then
+        begin
+          qoi_write_8(dst, QOI_OP_INDEX or index_pos);
         end
         else
         begin
-          qoi_write_8(dst, QOI_OP_RGBA);
-          qoi_write_8(dst, src.R);
-          qoi_write_8(dst, src.G);
-          qoi_write_8(dst, src.B);
-          qoi_write_8(dst, src.A);
+          index[index_pos] := src^;
+          if (src.A = px_prev.A) then
+          begin
+            vr := src.R - px_prev.R;
+            vg := src.G - px_prev.G;
+            vb := src.B - px_prev.B;
+            vg_r := vr - vg;
+            vg_b := vb - vg;
+            if ((vr > -3) and (vr < 2) and (vg > -3) and (vg < 2) and (vb > -3)
+              and (vb < 2)) then
+            begin
+              qoi_write_8(dst, QOI_OP_DIFF or (vr + 2) shl 4 or (vg + 2) shl 2 or
+                (vb + 2));
+            end
+            else if ((vg_r > -9) and (vg_r < 8) and (vg > -33) and (vg < 32) and
+              (vg_b > -9) and (vg_b < 8)) then
+            begin
+              qoi_write_8(dst, QOI_OP_LUMA or (vg + 32));
+              qoi_write_8(dst, (vg_r + 8) shl 4 or (vg_b + 8));
+            end
+            else
+            begin
+              qoi_write_8(dst, QOI_OP_RGB);
+              qoi_write_8(dst, src.R);
+              qoi_write_8(dst, src.G);
+              qoi_write_8(dst, src.B);
+            end
+          end
+          else
+          begin
+            qoi_write_8(dst, QOI_OP_RGBA);
+            qoi_write_8(dst, src.R);
+            qoi_write_8(dst, src.G);
+            qoi_write_8(dst, src.B);
+            qoi_write_8(dst, src.A);
+          end;
         end;
       end;
+      px_prev := src^;
+      inc(src);
     end;
-    px_prev := src^;
-    inc(src);
   end;
 
-  for i := 0 to 7 do
-    qoi_write_8(dst, qoi_padding[i]); // colorspace
+  if (run > 0) then
+    qoi_write_8(dst, QOI_OP_RUN or (run - 1));
+
+  for x := 0 to 7 do
+    qoi_write_8(dst, qoi_padding[x]); // colorspace
   max_size := dst - PByte(@bytes[0]);
   Stream.Write(bytes[0], max_size);
 end;
